f3fe37a0ceaccba95e1626efba584c39866de7a7
Abigail Limpioso <alimpioso@gmail.com>
2020-06-20 21:16:50 -0400

    fixed file path on server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const controller = require("./controller/employeeController.js");
+const controller = require("../controller/employeeController.js");
 const inquirer = require("inquirer");
-const Employee = require("./model/employees.js");
+const Employee = require("../model/employees.js");
 
 //Prompt for what action the user wants
 inquirer.prompt({
